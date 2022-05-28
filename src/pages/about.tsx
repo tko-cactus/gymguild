befa097ad88fb265cc5a008953db8957e8430bc1
tko-cactus <37374201+tko-cactus@users.noreply.github.com>
2022-05-28 23:29:55 +0900

    create next project

---
+++ src/pages/about.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const About: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>About page</title>
+      </Head>
+      <main>
+        <h1>This is about page</h1>
+      </main>
+    </div>
+  );
+};
+
+export default About;
